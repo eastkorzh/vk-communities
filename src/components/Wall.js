d2b5e8afd23ae3240bff37d65dad3534f35c2e45
Mikhail Korzhev <mkorzhev@hotmail.com>
2019-06-15 03:24:10 +0300

    implement pinterest-like posts layout

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Wall.sass'
-import { Link } from 'react-router-dom'
+import Post from './Post'
 
 class Wall extends React.Component {
+	componentDidMount() {
+		//console.log(this.content)
+	}
+
 	componentDidUpdate() {
 		const { state, props, wallGetRequest } = this.props
 
@@ -24,78 +28,45 @@ class Wall extends React.Component {
 	renderPosts() {
 		const { state, getCommentsRequest } = this.props
 
-		const takePhoto = (item, size) => {
-			if (!item.attachments) return
+		// const takePhoto = (item, size) => {
+		// 	if (!item.attachments) return
 			
-			let photos = []
+		// 	let photos = []
 	
-			for (let a of item.attachments) {
-				if (a.type === 'photo') photos.push(a.photo)
-			}
+		// 	for (let a of item.attachments) {
+		// 		if (a.type === 'photo') photos.push(a.photo)
+		// 	}
 	
-			if (!photos.length) return
+		// 	if (!photos.length) return
 	
-			const filteredPhotos =  photos.map(i => {
-				let result = {}
+		// 	const filteredPhotos =  photos.map(i => {
+		// 		let result = {}
 	
-				if (i.sizes[size]) {
-					result = i.sizes[size]	
-				} else {
-					for (let j=size; j>0; j--) {
-						if (i.sizes[j]) return i.sizes[j]
-					}
-				}
+		// 		if (i.sizes[size]) {
+		// 			result = i.sizes[size]	
+		// 		} else {
+		// 			for (let j=size; j>0; j--) {
+		// 				if (i.sizes[j]) return i.sizes[j]
+		// 			}
+		// 		}
 				
-				return result
-			})
+		// 		return result
+		// 	})
 			
-			return(
-				<img src={filteredPhotos[0].url} alt='' className='attached-img'/>
-			)
-		}
+		// 	return(
+		// 		<img src={filteredPhotos[0].url} alt='' className='attached-img'/>
+		// 	)
+		// }
 
-		const takeDate = (ms) => {
-			return new Date(ms).toLocaleString()
-		}
+		// const takeDate = (ms) => {
+		// 	return new Date(ms).toLocaleString()
+		// }
 
 		if (!state.isFatching && state.posts[0]) {
+
+
 			return state.posts.map(item => (
-				<Link to={`comments`} key={item.id} >
-					<div onClick={() => {
-						if (item.comments.count) {
-							sessionStorage.owner_id = item.owner_id
-							sessionStorage.item_id = item.id
-							getCommentsRequest(sessionStorage.owner_id, sessionStorage.item_id)
-						}
-						}} className='wall-post'>
-						<div className='post-date'>{takeDate(item.date*1000)}</div>
-						<div className='post-text'>{item.text}</div>
-						<div className='post-img-div'>
-							{takePhoto(item, 4)}
-						</div>
-						<div className='post-info'>
-							<div>
-								<div className='like-svg'/>
-								<div>{item.likes.count}</div>
-							</div>
-							{item.comments && 
-							<div>
-								<div className='comments-svg'/>
-								<div>{item.comments.count}</div>
-							</div>}
-							<div>
-								<div className='reposts-svg'/>
-								<div>{item.reposts.count}</div>
-							</div>
-							{item.views &&
-							<div className='views'>
-								<div className='view-svg'/>
-								<div>{item.views.count}</div>
-							</div>
-							}
-						</div>
-					</div>
-				</Link>
+				<Post item={item} getCommentsRequest={getCommentsRequest} key={item.id}/>
 			))
 		}
 	}
